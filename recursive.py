4745b70feda2dcbc2a62cb525c3f8bdaed45b2f7
mohammad <mr.dori.dev@gmail.com>
2021-11-19 20:32:40 +0330

    write the function

--- recursive.py
+++ recursive.py
@@ -3,3 +3,10 @@
 
 from sys import getsizeof
 from timeit import timeit
+
+def fibonacci(number: int) -> int:
+
+    if number in {0, 1}:
+        return number
+
+    return fibonacci(number - 1) + fibonacci(number - 2)
