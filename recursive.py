e4055ea6364c2f462495458805188209a93f2ccd
mohammad <mr.dori.dev@gmail.com>
2021-11-19 20:32:54 +0330

    add function docstring

--- recursive.py
+++ recursive.py
@@ -4,7 +4,16 @@
 from sys import getsizeof
 from timeit import timeit
 
+
 def fibonacci(number: int) -> int:
+    """calculate the fibonacci number of this index
+
+   Args:
+        number(int): index of fibonacci numbers
+
+    Returns:
+        int: fibonacci number
+    """
 
     if number in {0, 1}:
         return number
