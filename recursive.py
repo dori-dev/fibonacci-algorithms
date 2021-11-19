1c27addc6173055fcb5f45991ca71b88f018c3c3
mohammad <mr.dori.dev@gmail.com>
2021-11-19 20:33:26 +0330

    add test function

--- recursive.py
+++ recursive.py
@@ -19,3 +19,13 @@ def fibonacci(number: int) -> int:
         return number
 
     return fibonacci(number - 1) + fibonacci(number - 2)
+
+def test_fibonacci() -> bool:
+
+
+    fib_numbers = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    result = []
+    for number in range(len(fib_numbers)):
+        result.append(fibonacci(number))
+
+    return result == fib_numbers
