4cd7dcf7da0fe5cba4bf9a3a28c0bd93abc28b0f
mohammad <mr.dori.dev@gmail.com>
2021-12-04 17:59:40 +0330

    use static method

--- class.py
+++ class.py
@@ -17,35 +17,35 @@ class Fibonacci:
         self.cache.append(fib_number)
         return self.cache[number]
 
+    @staticmethod
+    def test_fibonacci() -> bool:
+        """test the fibonacci function
 
-def test_fibonacci() -> bool:
-    """test the fibonacci function
+        Returns:
+            bool: True if `fibonacci` currect work else False
+        """
+        fib_numbers = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+        result = []
+        for number in range(len(fib_numbers)):
+            result.append(fibonacci(number))
+        return fib_numbers == result
 
-    Returns:
-        bool: True if `fibonacci` currect work else False
-    """
-    fib_numbers = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-    result = []
-    for number in range(len(fib_numbers)):
-        result.append(fibonacci(number))
-    return fib_numbers == result
+    @staticmethod
+    def performance_test(numbers: int, codes: str, setup: str) -> None:
+        """test the performance of this fibonacci algorithm
 
-
-def performance_test(numbers: int, codes: str, setup: str) -> None:
-    """test the performance of this fibonacci algorithm
-
-    Args:
-        numbers (int): fibonacci numbers
-        codes (str): run codes
-        setup (str): setup codes
-    """
-    if test_fibonacci():
-        print('Correct Algorithm!')
-    else:
-        print('Incorrect Algorithm!')
-        return
-    print(getsizeof(numbers), 'bytes.')
-    print('time', timeit(codes, setup=setup, number=1000))
+        Args:
+            numbers (int): fibonacci numbers
+            codes (str): run codes
+            setup (str): setup codes
+        """
+        if test_fibonacci():
+            print('Correct Algorithm!')
+        else:
+            print('Incorrect Algorithm!')
+            return
+        print(getsizeof(numbers), 'bytes.')
+        print('time', timeit(codes, setup=setup, number=1000))
 
 
 COUNT = 10
