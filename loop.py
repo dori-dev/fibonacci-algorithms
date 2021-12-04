5a20e42603de9761370c86064f63d07c903c937c
mohammad <mr.dori.dev@gmail.com>
2021-12-04 18:17:19 +0330

    delete check input in function

--- loop.py
+++ loop.py
@@ -7,10 +7,6 @@ from timeit import timeit
 
 
 def fibonacci(number):
-
-    if not (isinstance(number, int) and number >= 0):
-        raise ValueError("Please enter a positive interger!")
-
     first, second = 0, 1
     data = []
     for _ in range(1, number+1):
