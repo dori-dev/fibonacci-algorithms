e03efc1af8860831446327ee77e0c7e1477d8b8e
mohammad <mr.dori.dev@gmail.com>
2021-12-04 17:55:57 +0330

    add class

--- class.py
+++ class.py
@@ -4,3 +4,15 @@
 # Standart library imports
 from sys import getsizeof
 from timeit import timeit
+
+
+class Fibonacci:
+    def __init__(self):
+        self.cache = [0, 1]
+
+    def __call__(self, number):
+        if number < len(self.cache):
+            return self.cache[number]
+        fib_number = self(number - 1) + self(number - 2)
+        self.cache.append(fib_number)
+        return self.cache[number]
