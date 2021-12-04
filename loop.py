5d9a626cd2e555f5270e236275042049d36933d1
mohammad <mr.dori.dev@gmail.com>
2021-12-04 18:15:36 +0330

    add fibonacci function

--- loop.py
+++ loop.py
@@ -4,3 +4,17 @@
 # Standart library imports
 from sys import getsizeof
 from timeit import timeit
+
+
+def fibonacci(number):
+
+    if not (isinstance(number, int) and number >= 0):
+        raise ValueError("Please enter a positive interger!")
+
+    first, second = 0, 1
+    data = []
+    for _ in range(1, number+1):
+        data.append(first)
+        first, second = second, first + second
+
+    return data
