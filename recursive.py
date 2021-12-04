a8359b4a9ef05a979a4d1462ebfd35ea61a600af
mohammad <mr.dori.dev@gmail.com>
2021-12-04 17:27:46 +0330

    cleaning code

--- recursive.py
+++ recursive.py
@@ -15,10 +15,8 @@ def fibonacci(number: int) -> int:
     Returns:
         int: fibonacci number
     """
-
     if number in {0, 1}:
         return number
-
     return fibonacci(number - 1) + fibonacci(number - 2)
 
 
@@ -28,12 +26,10 @@ def test_fibonacci() -> bool:
     Returns:
         bool: True if `fibonacci` currect work else False
     """
-
     fib_numbers = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
     result = []
     for number in range(len(fib_numbers)):
         result.append(fibonacci(number))
-
     return fib_numbers == result
 
 
@@ -45,22 +41,18 @@ def performance_test(numbers: int, codes: str, setup: str) -> None:
         codes (str): run codes
         setup (str): setup codes
     """
-
     if test_fibonacci():
-        print('Correct algorithm!')
+        print('Correct Algorithm!')
     else:
-        print('Incorrect algorithm!')
+        print('Incorrect Algorithm!')
         return
-
     print(getsizeof(numbers), 'bytes.')
-
     print('time', timeit(codes, setup=setup, number=1000))
 
 
 COUNT = 10
 if not (isinstance(COUNT, int) and COUNT >= 0):
     raise ValueError("Please enter a positive interger!")
-
 fibonacci_numbers = (fibonacci(number) for number in range(COUNT))
 print('fibonacci(10) ->', *fibonacci_numbers)
 
