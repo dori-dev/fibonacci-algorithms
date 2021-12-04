a45e746e5e2719242fcae7648612d4d6f919da27
mohammad <mr.dori.dev@gmail.com>
2021-12-04 17:31:28 +0330

    add function

--- cache.py
+++ cache.py
@@ -5,3 +5,23 @@
 # Standart library imports
 from sys import getsizeof
 from timeit import timeit
+
+
+cache = {
+    0: 0,
+    1: 1
+}
+
+
+def fibonacci(number: int) -> int:
+    """calculate fibonacci of the number
+
+   Args:
+        number(int): number with calculate it fibonacci
+
+    Returns:
+        int: fibonacci(number - 1) + fibonacci(number -2)  recursive case
+    """
+    if number not in cache:
+        cache[number] = fibonacci(number - 1) + fibonacci(number - 2)
+    return cache[number]
